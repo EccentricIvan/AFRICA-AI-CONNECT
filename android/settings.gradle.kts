982c2b7612c4f711f3caea1f0081b12eec8e6a52
ESIANA EMMANUEL <esiana.emmah@gmail.com>
2026-08-13 14:52:35 +0300

    Fix Android APK build configuration

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,36 +1,26 @@
-pluginManagement {
-        val flutterSdkPath = run {
-                    val properties = java.util.Properties()
-                            file("local.properties").inputStream().use { properties.load(it) }
-                                    val flutterSdkPath = properties.getProperty("flutter.sdk")
-                                            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-                                                    flutterSdkPath
-        }
+﻿pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        flutterSdkPath
+    }
 
-            includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
-                repositories {
-                            google()
-                                    mavenCentral()
-                                            gradlePluginPortal()
-                }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 
 plugins {
-        id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-
-            // Compatible with the Flutter version used by CI
-                id("com.android.application") version "8.11.1" apply false
-
-                    // Firebase
-                        id("com.google.gms.google-services") version "4.3.15" apply false
-
-                            // Flutter 3.47 requires Kotlin >= 2.2.20
-                                id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.11.1" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 include(":app")
-}
-                }
-        }
-}
